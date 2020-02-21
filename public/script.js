bd83bb05b56d413dc4ac1c8e92c7e65c46ebde73
Irfan <53381250+mdirfan-haja@users.noreply.github.com>
2020-02-21 20:49:30 +0000

    update

--- public/script.js
+++ public/script.js
@@ -1,14 +1,66 @@
 console.log('connected!!')
+const db = firebase.firestore()
+const customersList = document.getElementById('customerslist')
+const applicationForm = document.getElementById('application1')
+    //const submitForm = document.getElementById('submitBtn1')
+    //var submitBtn1 = document.getElementById('submitBtn1')
+    //var maintext = document.getElementById('maintext')
+    //var postCode = document.getElementById('postcodeText')
+    //var homeAdderss = document.getElementById('addressText')
+
+function renderCustmoers(doc) {
+    let span = document.createElement('tr')
+    let fullname = document.createElement('th')
+    let address = document.createElement('th')
+    let postCode = document.createElement('th')
 
 
-var submitBtn1 = document.getElementById('submitBtn1')
+    span.setAttribute('data-id', doc.id)
+    fullname.textContent = doc.data().fullname
+    address.textContent = doc.data().Address
+    postCode.textContent = doc.data().postcode
 
+    span.appendChild(fullname)
+    span.appendChild(address)
+    span.appendChild(postCode)
 
+    customersList.appendChild(span)
+}
+
+//getting data from firestors
+db.collection('customers').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        renderCustmoers(doc)
+    })
+})
+
+//saving data to firestore
 function datasubmitbtn() {
-    var database = firebase.database().ref();
-    var maintext = document.getElementById('maintext').value
-
-    database.push().set(maintext)
-        //database.child('text').set(maintext)
+    applicationForm.addEventListener('submit', (e) => {
+        e.preventDefault
+    })
+    db.collection('customers').add({
+        fullname: applicationForm.fullname1.value,
+        Address: applicationForm.address1.value
+    })
 
 }
+
+
+
+
+
+
+/*
+    function datasubmitbtn() {
+        var database = firebase.database().ref();
+        var commentText = maintext.value
+        var postcodeText = postCode.value
+
+        database.push().set(commentText)
+        database.child('Postcode').push().set(postcodeText)
+
+        //database.child('text').set(maintext)
+
+    }
+    */
